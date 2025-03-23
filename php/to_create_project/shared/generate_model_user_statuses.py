5ad1259f2f8f7d62ea7142be87ecfaca5bba39c7
dilandarren <dilandarren@gmail.com>
2025-03-23 20:28:51 +0100

    add models

--- php/to_create_project/shared/generate_model_user_statuses.py
+++ php/to_create_project/shared/generate_model_user_statuses.py
@@ -3,12 +3,6 @@ from helpers.helper_print import print_message, GREEN, CYAN
 
 
 
-def generate_model_user_statuses(full_path):
-    pass
-
-
-
-
 
 def generate_model_user_statuses(full_path):
     """
