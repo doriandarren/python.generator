2753d6ff51110580d26074194e95e4c3d84ff523
mile <darimile@gmail.com>
2025-03-23 19:06:48 +0100

    fix ability update

--- php/to_create_project/shared/generate_controller_abilities.py
+++ php/to_create_project/shared/generate_controller_abilities.py
@@ -117,7 +117,99 @@ def create_store(full_path):
     file_path = os.path.join(styles_path, "AbilityStoreController.php")
 
     # Contenido por defecto
-    content = """
+    content = """<?php
+
+namespace App\\Http\\Controllers\\SHARED\\Abilities;
+
+use App\\Http\\Controllers\\Controller;
+use App\\Repositories\\Abilities\\AbilityRepository;
+use Illuminate\\Http\\JsonResponse;
+use Illuminate\\Http\\Request;
+use Illuminate\\Support\\Facades\\Validator;
+
+class AbilityStoreController extends Controller
+{
+
+	private AbilityRepository $repository;
+
+
+	public function __construct()
+	{
+		$this->repository = new AbilityRepository();
+	}
+
+	/**
+	* @header Bearer BEARER_AUTH
+	*
+	* @bodyParam ability_group_id string required
+	* @bodyParam name string required
+	* @bodyParam label string required
+	*
+	* @param Request $request
+	* @return JsonResponse
+	*/
+	public function __invoke(Request $request): JsonResponse
+	{
+
+		if($this->isAdmin(auth()->user()->roles)){
+
+			// By Admin
+
+			$validator = Validator::make($request->all(), [
+				'ability_group_id'=>'required',
+				'name'=>'required',
+				'label'=>'required',
+			]);
+			if($validator->fails()){
+				return $this->respondWithError('Error', $validator->errors());
+			}
+			$ability = $this->repository->setAbility($request->ability_group_id, $request->name, $request->label);
+
+			$data = $this->repository->store($ability);
+
+			return $this->respondWithData('Ability created', $data);
+
+		}else if($this->isManager(auth()->user()->roles)){
+
+			// By Manager
+
+			$validator = Validator::make($request->all(), [
+				'ability_group_id'=>'required',
+				'name'=>'required',
+				'label'=>'required',
+			]);
+			if($validator->fails()){
+				return $this->respondWithError('Error', $validator->errors());
+			}
+			$ability = $this->repository->setAbility($request->ability_group_id, $request->name, $request->label);
+
+			$data = $this->repository->store($ability);
+
+			return $this->respondWithData('Ability created', $data);
+
+		}else{
+
+			// By Role User
+
+			$validator = Validator::make($request->all(), [
+				'ability_group_id'=>'required',
+				'name'=>'required',
+				'label'=>'required',
+			]);
+			if($validator->fails()){
+				return $this->respondWithError('Error', $validator->errors());
+			}
+			$ability = $this->repository->setAbility($request->ability_group_id, $request->name, $request->label);
+
+			$data = $this->repository->store($ability);
+
+			return $this->respondWithData('Ability created', $data);
+
+		}
+
+	}
+
+}
 """
 
     try:
@@ -276,6 +368,114 @@ class AbilityShowController extends Controller
         print_message(f"Error al generar el archivo {file_path}: {e}", CYAN)
 
 
+def create_update(full_path):
+    """
+    Genera un archivo
 
-333
+    Args:
+        full_path (str): Ruta completa del proyecto.
+    """
+    styles_path = os.path.join(full_path, "app", "Http", "Controllers", "API", "SHARED", "abilities")
+
+    # Crear la carpeta si no existe
+    if not os.path.exists(styles_path):
+        os.makedirs(styles_path)
+        print_message(f"Carpeta creada: {styles_path}", GREEN)
+
+    # Ruta completa del archivo
+    file_path = os.path.join(styles_path, "AbilityUpdateController.php")
+
+    # Contenido por defecto
+    content = """<?php
+
+namespace App\\Http\\Controllers\\SHARED\\Abilities;
+
+use App\\Http\\Controllers\\Controller;
+use App\\Models\\Abilities\\Ability;
+use App\\Repositories\\Abilities\\AbilityRepository;
+use Illuminate\\Http\\JsonResponse;
+use Illuminate\\Http\\Request;
+use Illuminate\\Support\\Facades\\Validator;
+
+class AbilityUpdateController extends Controller
+{
+
+	private AbilityRepository $repository;
+
+
+	public function __construct()
+	{
+		$this->repository = new AbilityRepository();
+	}
+
+	/**
+	* @header Bearer BEARER_AUTH
+	*
+	* @bodyParam ability_group_id string required
+	* @bodyParam name string required
+	* @bodyParam label string required
+	*
+	* @param Request $request
+	* @param Ability $ability
+	* @return JsonResponse
+	*/
+	public function __invoke(Request $request, Ability $ability): JsonResponse
+	{
+
+		if($this->isAdmin(auth()->user()->roles)){
+			// By Admin
+
+			$validator = Validator::make($request->all(), [
+				'ability_group_id'=>'required',
+				'name'=>'required',
+				'label'=>'required',
+			]);
+
+			if($validator->fails()){
+				return $this->respondWithError('Error', $validator->errors());
+			}
+
+			$data = $this->repository->update($ability->id, $request->all());
+			return $this->respondWithData('Ability updated', $data);
+
+		}else{
+
+			// By Role User
+
+			$validator = Validator::make($request->all(), [
+				'ability_group_id'=>'required',
+				'name'=>'required',
+				'label'=>'required',
+			]);
+
+			if($validator->fails()){
+				return $this->respondWithError('Error', $validator->errors());
+			}
+
+			if($ability->company_id == auth()->user()->employee->company_id){
+
+				$data = $this->repository->update($ability->id, $request->all());
+
+				return $this->respondWithData('Ability updated', $data);
+
+			}else{
+
+				return $this->respondWithError('Error', ['e' => trans('validation.user_not_belong_company')]);
+
+			}
+
+		}
+
+	}
+
+}
+"""
+
+    try:
+        # Crear o sobrescribir el archivo con el contenido
+        with open(file_path, "w") as f:
+            f.write(content)
+        print_message(f"Archivo generado: {file_path}", GREEN)
+    except Exception as e:
+        print_message(f"Error al generar el archivo {file_path}: {e}", CYAN)
 
