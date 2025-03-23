75ed50c8dfa5a3d2ac8dd1d832923c3a89b7ea9e
mile <darimile@gmail.com>
2025-03-23 18:57:57 +0100

    test mile

--- php/to_create_project/shared/generate_controller_abilities.py
+++ php/to_create_project/shared/generate_controller_abilities.py
@@ -99,6 +99,36 @@ class AbilityDestroyController extends Controller
 
 
 
+def create_store(full_path):
+    """
+    Genera un archivo
+
+    Args:
+        full_path (str): Ruta completa del proyecto.
+    """
+    styles_path = os.path.join(full_path, "app", "Http", "Controllers", "API", "SHARED", "Abilities")
+
+    # Crear la carpeta si no existe
+    if not os.path.exists(styles_path):
+        os.makedirs(styles_path)
+        print_message(f"Carpeta creada: {styles_path}", GREEN)
+
+    # Ruta completa del archivo
+    file_path = os.path.join(styles_path, "AbilityStoreController.php")
+
+    # Contenido por defecto
+    content = """
+"""
+
+    try:
+        # Crear o sobrescribir el archivo con el contenido
+        with open(file_path, "w") as f:
+            f.write(content)
+        print_message(f"Archivo generado: {file_path}", GREEN)
+    except Exception as e:
+        print_message(f"Error al generar el archivo {file_path}: {e}", CYAN)
+
+
 def create_list(full_path):
     """
     Genera un archivo
@@ -246,4 +276,6 @@ class AbilityShowController extends Controller
         print_message(f"Error al generar el archivo {file_path}: {e}", CYAN)
 
 
+
+333
 
