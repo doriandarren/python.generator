95313cf1d3ae679676fa3a7adf4d6c4b9af16c43
Dorian Gonzalez <doriandarren1@gmail.com>
2025-03-23 19:22:06 +0100

    fix countries roles statuses

---
+++ php/to_create_project/shared/generate_controller_role_users.py
@@ -0,0 +1,17 @@
+import os
+from helpers.helper_print import print_message, GREEN, CYAN
+
+
+
+def generate_controller_roles(full_path):
+    create_destroy(full_path)
+    create_list(full_path)
+    create_show(full_path)
+    create_store(full_path)
+    create_update(full_path)
+
+
+
+
+
+
