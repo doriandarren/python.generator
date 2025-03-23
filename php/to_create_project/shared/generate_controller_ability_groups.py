e88d360972795253e9dfef2d90831369eefdd6d9
Dorian Gonzalez <doriandarren1@gmail.com>
2025-03-23 19:05:47 +0100

    fix ability_groups

--- php/to_create_project/shared/generate_controller_ability_groups.py
+++ php/to_create_project/shared/generate_controller_ability_groups.py
@@ -5,6 +5,10 @@ from helpers.helper_print import print_message, GREEN, CYAN
 
 def generate_controller_ability_groups(full_path):
     create_destroy(full_path)
+    create_list(full_path)
+    create_show(full_path)
+    create_store(full_path)
+    create_update(full_path)
 
 
 
