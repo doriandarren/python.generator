0f63283db4aab4a6189df830fdd192217fdbf7c8
Dorian Gonzalez <doriandarren1@gmail.com>
2025-03-23 18:20:11 +0100

    add php generate controllers

---
+++ php/to_create_project/shared/generate_controller_ability_users.py
@@ -0,0 +1,39 @@
+import os
+from helpers.helper_print import print_message, GREEN, CYAN
+
+
+
+def generate_controller_ability_users(full_path):
+    create_destroy(full_path)
+
+
+
+def create_destroy(full_path):
+    """
+    Genera un archivo
+
+    Args:
+        full_path (str): Ruta completa del proyecto.
+    """
+    styles_path = os.path.join(full_path, "app", "Http", "Controllers", "API", "SHARED", "Abilities")
+
+    # Crear la carpeta si no existe
+    if not os.path.exists(styles_path):
+        os.makedirs(styles_path)
+        print_message(f"Carpeta creada: {styles_path}", GREEN)
+
+    # Ruta completa del archivo
+    file_path = os.path.join(styles_path, ".php")
+
+    # Contenido por defecto
+    content = """
+"""
+
+    try:
+        # Crear o sobrescribir el archivo con el contenido
+        with open(file_path, "w") as f:
+            f.write(content)
+        print_message(f"Archivo generado: {file_path}", GREEN)
+    except Exception as e:
+        print_message(f"Error al generar el archivo {file_path}: {e}", CYAN)
+
