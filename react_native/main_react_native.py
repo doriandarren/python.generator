caa23ccb820b8d6b8bc3a20a2cc9e226ac975591
Milena <darimile@gmail.com>
2025-12-21 00:02:04 +0100

    fix main react native

--- react_native/main_react_native.py
+++ react_native/main_react_native.py
@@ -1,3 +1,4 @@
+
 import sys
 import os
 
@@ -10,38 +11,39 @@ from helpers.helper_menu import menu_list, clear_screen
 from react_native.to_create_module_crud.start_module_crud_react_native import start_module_crud_react_native
 from react_native.to_create_project.start_project_react_native import start_project_react_native
 
+
 def main_react_native():
     """Menú principal para generar código (proyectos / módulos)."""
 
     while True:
         clear_screen()
-        print_header("React_Native")
+        print_header("React Native")
 
         str_input = menu_list(
             "¿Qué quieres crear?: ",
-            ["Proyecto", "Modulo", "<-Back"]
+            [
+                {"name": "Proyecto", "value": "project"},
+                {"name": "Módulo CRUD", "value": "crud"},
+                {"name": "Volver", "value": "back"},
+            ]
         )
 
         opt = str_input.strip().lower()
 
         print(f"Crear un: {str_input} ")
 
-        if opt.startswith('proyecto'):
-            pass
-            start_project_react_native() ## TODO refactor
-
-        elif opt.startswith("módulo") or opt.startswith("modulo"):
-            pass
-            start_module_crud_react_native() ## TODO refactor
-
-        elif opt.startswith("<-") or opt.startswith("back"):
+        if opt == 'project':
+            start_project_react_native()
+            
+        elif opt == 'crud':
+            start_module_crud_react_native()
+            
+        elif opt == 'back':
             print("\nVolviendo al menú anterior...\n")
             break
 
         else:
             print("Opción no reconocida.")
-
-        
 
 
 if __name__ == "__main__":
