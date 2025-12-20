1a430d588b90a497d19c743867fbc11470b282ef
Dorian Gonzalez <doriandarren1@gmail.com>
2025-12-20 23:50:41 +0100

    add todo

--- cpp/main_cpp.py
+++ cpp/main_cpp.py
@@ -45,3 +45,61 @@ def main_cpp():
 
 if __name__ == "__main__":
     main_cpp()
+
+
+
+
+
+
+
+
+
+import sys
+import os
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if BASE_DIR not in sys.path:
+    sys.path.append(BASE_DIR)
+
+from helpers.helper_print import print_header
+from helpers.helper_menu import menu_list, clear_screen
+
+
+def main_cpp():
+    """Menú principal para generar código (proyectos / módulos)."""
+
+    while True:
+        clear_screen()
+        print_header("main_cpp")
+
+        str_input = menu_list(
+            "¿Qué quieres crear?: ",
+            [
+                {"name": "Proyecto", "value": "project"},
+                {"name": "Módulo CRUD", "value": "crud"},
+                {"name": "Volver", "value": "back"},
+            ]
+        )
+
+        opt = str_input.strip().lower()
+
+        print(f"Crear un: {str_input} ")
+
+        if opt == 'project':
+            pass
+            # start_project_react()
+            
+        elif opt == 'crud':
+            pass
+            # start_module_react()
+            
+        elif opt == 'back':
+            print("\nVolviendo al menú anterior...\n")
+            break
+
+        else:
+            print("Opción no reconocida.")
+
+
+if __name__ == "__main__":
+    main_cpp()
