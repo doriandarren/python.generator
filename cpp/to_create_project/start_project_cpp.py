0fba147d9637ab849d104aeed6a025eea48cb0e3
Dorian Gonzalez <doriandarren1@gmail.com>
2025-12-21 21:41:11 +0100

    fix todo

--- cpp/to_create_project/start_project_cpp.py
+++ cpp/to_create_project/start_project_cpp.py
@@ -1,5 +1,3 @@
-
-
 from helpers.helper_columns import parse_columns_input
 from helpers.helper_menu import menu_checkbox, pause
 from helpers.helper_print import input_with_validation
@@ -7,20 +5,24 @@ from helpers.helper_print import input_with_validation
 
 def start_project_cpp():
     
-    # Ruta predeterminada
-    default_path = "/Users/dorian/PhpstormProjects81"
+    # Defaults
+    default_path = "/Users/dorian/ReactProjects"
+    default_name = "app-1"
+    
+    # Inputs
+    project_name = input_with_validation(f"Nombre del proyecto (defacto: {default_name}): ", default_name)
+    project_path = input_with_validation(f"Ruta para crear el proyecto (defecto: {default_path}): ",  default_path)
 
-    project_name = input_with_validation("Nombre del proyecto: ", None)
-    project_path = input(f"Ruta para crear el proyecto (por defecto: {default_path}): ").strip()
 
-    # Si no se introduce una ruta, usar la predeterminada
-    if not project_path:
-        project_path = default_path
-
-    # Combinar la ruta y el nombre del proyecto
+    # Split
     full_path = f"{project_path}/{project_name}"
     
     
-    ## TODO irian las llamadas a los metodos para crear el proyecto
+    ## TODO llamadas a los "generate_"
+    # generate_by_command_line(full_path)
+    
+    
+    
+
 
     
